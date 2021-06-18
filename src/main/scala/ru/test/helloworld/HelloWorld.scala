0efb8628023adb8f507c341e438885fba063258b
LenyTao <nrv5@yandex.ru>
2021-06-18 18:23:44 +0400

    Test commit10

--- src/main/scala/ru/test/helloworld/HelloWorld.scala
+++ src/main/scala/ru/test/helloworld/HelloWorld.scala
@@ -4,6 +4,7 @@ import ru.test.helloworld.classes.Person
 
 object HelloWorld extends App {
   val person = Person("Boris",55)
+  val person2 = Person("Zhen",55)
 
   println(s"Hello World ${person.getPerson()}!")
 }
