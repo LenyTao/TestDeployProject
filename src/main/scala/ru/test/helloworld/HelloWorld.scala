551fd2ac00cbdc389315818b5170d6b4eca05a93
LenyTao <nrv5@yandex.ru>
2021-06-28 22:26:39 +0400

    Add person3

--- src/main/scala/ru/test/helloworld/HelloWorld.scala
+++ src/main/scala/ru/test/helloworld/HelloWorld.scala
@@ -5,6 +5,6 @@ import ru.test.helloworld.classes.Person
 object HelloWorld extends App {
   val person = Person("Elisei",55)
   val person2 = Person("Zhen",55)
-
+val person3 = Person("Adam",28)
   println(s"Hello World ${person.getPerson()}!")
 }
