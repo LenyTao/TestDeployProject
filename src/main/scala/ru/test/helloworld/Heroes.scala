c2a7286b268b74a9c54b7eabb0e1c1eb6da9711e
LenyTao <nrv5@yandex.ru>
2021-06-28 22:57:24 +0400

    Add class Heroes and add 3 heroes

---
+++ src/main/scala/ru/test/helloworld/Heroes.scala
@@ -0,0 +1,9 @@
+package ru.test.helloworld
+
+import ru.test.helloworld.classes.Person
+
+object Heroes {
+  val hero1 = Person("Spider-Man", 1)
+  val hero2 = Person("Superman", 2)
+  val hero3 = Person("Batman", 3)
+}
